01087a31f216b0cfa4cb2ee1c551fc0d1c4ca356
Sam Martin <sammartin@MacBook-Pro-de-Sam.local>
2024-04-01 16:28:58 -0300

    novos tipos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 const { gql, ApolloServer } = require("apollo-server");
 
+/**
+ * Scalar types
+ * - Int
+ * - Float
+ * - String
+ * - Boolean
+ * - ID
+ */
+
+/**
+ * => Schema
+ * -> Schema Definition Language
+ * -> SDL
+ */
+
 const resolvers = {
     Query: {
         hello() {
             return "World";
+        },
+        usuario() {
+            return {
+                idade: 32,
+                salario: 7524.40,
+                nome: "Samuel",
+                ativo: true,
+                id: "0s1a2m3u4e5l"
+            }
+        },
+        linguagens() {
+            return ["C", "C#", "Java", "Python"]
         }
     }
 }
+
 const typeDefs = gql`
     type Query {
         hello: String
+        usuario: User
+        linguagens: [String!]!
+    }
+    type User {
+        idade: Int
+        salario: Float
+        nome: String
+        ativo: Boolean
+        id: ID
     }
 `
 
